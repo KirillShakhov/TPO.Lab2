94d0d28182c2f7729de74d867e5d51a43d7e25f5
kirill <kirill.shakhov14@gmail.com>
2022-04-18 16:46:51 +0300

    add CscWritter test

--- src/test/java/ru/ifmo/tpo/lab2/AppTest.java
+++ src/test/java/ru/ifmo/tpo/lab2/AppTest.java
@@ -1,19 +1,14 @@
 package ru.ifmo.tpo.lab2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.io.IOException;
+
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    // @Test
-    // public void shouldAnswerWithTrue()
-    // {
-    //     assertEquals(0, App.test());;
-    // }
+    @Test
+    public void shouldAnswerWithTrue() throws IOException {
+        App.main(null);
+    }
 }
--- src/test/java/ru/ifmo/tpo/lab2/CscWritterTests.java
+++ src/test/java/ru/ifmo/tpo/lab2/CscWritterTests.java
@@ -1,5 +1,39 @@
 package ru.ifmo.tpo.lab2;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CscWritterTests {
-    
+    @Test
+    public void checkCscWritter() throws IOException {
+        Double from = -1D;
+        Double to = 1D;
+        Double step = 1D;
+        String path = "csv/test_csv.csv";
+
+        CsvWriter.write(
+                path,
+                x -> Math.sin(x),
+                from,
+                to,
+                step);
+
+        File f = new File(path);
+        assertTrue(f.exists() && !f.isDirectory());
+        StringBuilder res = new StringBuilder();
+        Scanner input = new Scanner(f);
+        while (input.hasNextLine())
+        {
+            res.append(input.nextLine()).append("\n");
+        }
+        assertEquals("-1.0,-0.8414709848078965\n" +
+                "0.0,0.0\n" +
+                "1.0,0.8414709848078965\n", res.toString());
+    }
 }
